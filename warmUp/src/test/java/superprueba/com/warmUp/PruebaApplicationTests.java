74c57b0aed20dbedd81c0781f0e902e4d8a579e5
aledeleonez <aledeleonez@gmail.com>
2022-02-01 18:29:47 +0100

    Mi primera aplicación con Spring

---
+++ warmUp/src/test/java/superprueba/com/warmUp/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package superprueba.com.warmUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
